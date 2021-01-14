43e3ecc8ac972e4d344aef343e2baded74d7f3a3
Frankie <zz1763@nyu.edu>
2021-01-14 15:52:24 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -4,12 +4,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=15000
+#SBATCH --mem=22GB
 #SBATCH --gres=gpu:aquila
 #SBATCH --time=50:00:00
 #SBATCH --mail-type=END
+#SBATCH --mail-user=email@address # put your email here if you want emails
 #SBATCH --output=deeplab_%j.out
-#SBATCH --error=DL_error_%j.out
+#SBATCH --error=deeplab_%j.err
 #SBATCH --gres=gpu:1 # How much gpu need, n is the number
 #SBATCH -p aquila
 
