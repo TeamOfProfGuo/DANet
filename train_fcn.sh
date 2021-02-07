c6ca6db49df0db537dab08fba5691f4f07915188
Frankie <zz1763@nyu.edu>
2021-02-07 09:56:30 +0800

    conflicts solved

--- train_fcn.sh
+++ train_fcn.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-#SBATCH --job-name=deepLabv2
+#SBATCH --job-name=fcn
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=15000
+#SBATCH --mem=22GB
 #SBATCH --gres=gpu:aquila
 #SBATCH --time=50:00:00
 #SBATCH --mail-type=END
-#SBATCH --output=deeplab_test_%j.out
-#SBATCH --error=test_error_%j.err
+#SBATCH --mail-user=email@address # put your email here if you want emails
+#SBATCH --output=deeplab_%j.out
+#SBATCH --error=deeplab_%j.err
 #SBATCH --gres=gpu:1 # How much gpu need, n is the number
 #SBATCH -p aquila
 
 module purge
-module activate anaconda3
+module load anaconda3
+# use your own env name
 module load cuda/10.0
 module load gcc/7.3
 cd /gpfsnyu/scratch/zz1763/DeepLearning/DANet 
+echo "start training"
 source activate dl
-echo "start testing"
-module load cuda/10.0
-module load gcc/7.3
-
-python test.py
+python train.py --config './results/fcn_resnet50/config.yaml'
 echo "FINISH"
 
